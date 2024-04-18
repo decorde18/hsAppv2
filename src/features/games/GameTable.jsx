c864db26bc0abd158289e0065bd15c3cc02ce37a
David Cordero de Jesus <decorde@yahoo.com>
2024-04-18 17:39:50 -0500

    fix broken link for add game

--- src/features/games/GameTable.jsx
+++ src/features/games/GameTable.jsx
@@ -37,7 +37,7 @@ function GameTable() {
         />
         <div>{filteredCount} games</div>
         <Modal>
-          {!session?.provider_token ||
+          {!session.provider_token ||
           !process.env.NODE_ENV ||
           process.env.NODE_ENV !== 'development' ? (
             <Modal.Open opens="game-form-error">
